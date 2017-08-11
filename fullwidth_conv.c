a75c1fd2055ede2adff899f8dbd5aafa68cab8e5
James Preiss <japreiss@usc.edu>
2017-08-11 13:19:36 -0700

    code comment, .gitignore

--- fullwidth_conv.c
+++ fullwidth_conv.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// write fullwidth version of ascii character to stdout in UTF-8 encoding.
 void writefw(char c)
 {
 	uint16_t delta = 0xFF01u - '!';
 
 	if (c >= '!' && c <= '~') {
 		uint16_t fw = delta + c;
-		uint8_t utf8[3];
 		putchar(0xE0 | (fw >> 12));
 		putchar(0x80 | (0x3F & (fw >> 6)));
 		putchar(0x80 | (0x3F & fw));
